3da55fef24bbf74602709a8f47a7dcda6f0e0911
Marcuth <thiago.playgames15@gmail.com>
2026-09-02 00:29:48 -0300

    refactor: properly configure BigjpgError class

--- src/error.ts
+++ src/error.ts
@@ -1,1 +1,7 @@
-export class BigjpgError extends Error {}
+export class BigjpgError extends Error {
+    constructor(message?: string) {
+        super(message)
+        this.name = "BigjpgError"
+        Object.setPrototypeOf(this, new.target.prototype)
+    }
+}
